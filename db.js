34e1399588391c1b9c4758cd3c52d79952d57cdb
Tablu <tablu@Tablus-MacBook-Pro.local>
2024-09-25 12:05:41 +0530

    minor change

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise')
 require("dotenv").config();
 
-const mysqlPool =mysql.createPool({
-    host:process.env.HOST,
-    user:process.env.USER,
-    password:process.env.PASSWORD,
-    database:process.env.DATABASE
+const mysqlPool = mysql.createPool({
+    host: process.env.HOST,
+    user: process.env.DBUSER,
+    password: process.env.DBPASSWORD,
+    database: process.env.DATABASE
 })
 
 module.exports = mysqlPool
